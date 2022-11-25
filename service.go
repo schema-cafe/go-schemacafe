d99a8af9a06135f18f6bf1bb01c8d3764c85824d
Michael Rybka <merybka@gmail.com>
2022-11-25 14:04:04 -0500

    Serve files from data dir

--- service.go
+++ service.go
@@ -1,8 +1,20 @@
 package schemacafe
 
-import "net/http"
+import (
+	"net/http"
+	"path/filepath"
 
-type Service struct{}
+	"github.com/library-development/go-web"
+)
+
+type Service struct {
+	DataDir string `json:"dataDir"`
+}
 
 func (s *Service) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	web.HandleCORS(w, r)
+	switch r.Method {
+	case "GET":
+		http.ServeFile(w, r, filepath.Join(s.DataDir, r.URL.Path))
+	}
 }
